b1576a76d8e107a914b856ce1443caeed06dfd87
Jonathan Leo <jpleo122@gmail.com>
2021-01-28 17:47:37 -0500

    Initial vertex shader testing commit

--- slicingtest/slicingtest/ViewController.swift
+++ slicingtest/slicingtest/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet var sceneView: ARSCNView!
     @IBOutlet weak var addButton: UIButton!
     
-    var decodeTube: SCNNode!
+    var tubeNode: SCNNode!
     var planeNode: SCNNode!
     var heartNode: SCNNode!
     var planeEquation: simd_float4!
@@ -33,10 +33,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         self.sceneView.autoenablesDefaultLighting = true
         
         // Show statistics such as fps and timing information
-//        sceneView.showsStatistics = true
+        // sceneView.showsStatistics = true
         
         // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        // let scene = SCNScene(named: "art.scnassets/ship.scn")!
         let scene = SCNScene()
         
         // Set the scene to the view
@@ -91,20 +91,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.scene.rootNode.addChildNode(sphereNode)
     }
     
-    func updateSlicingPlane(refNode: SCNNode) {
-        // update plane equation
-        let normal = (planeNode.simdOrientation * simd_quatf(real: 0, imag: initNormal)) * planeNode.simdOrientation.inverse
-        self.planeEquation = simd_float4(normal.imag, simd_dot(normal.imag, planeNode.simdPosition))
-        
-        // delete sliced models currently in scene
-        sceneView.scene.rootNode.childNodes.filter({ $0.name == "slicedNode" }).forEach({ $0.removeFromParentNode() })
-        
-        // update sliced model and re add to scene
-        let node = decodeGeom(node: refNode)!
-        sceneView.scene.rootNode.addChildNode(node)
-        node.transform = refNode.transform
-    }
-    
     @objc func panGesture(sender: UIPanGestureRecognizer) {
         if planeNode != nil {
             self.planeNode.opacity = 1
@@ -124,43 +110,32 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                 currentAngleX = newAngleX
                 currentAngleY = newAngleY
             }
-
 //             update plane equation
 //            let normal = (planeNode.simdOrientation * simd_quatf(real: 0, imag: initNormal)) * planeNode.simdOrientation.inverse
 //            planeEquation = simd_float4(normal.imag, simd_dot(normal.imag, planeNode.simdPosition))
 //            print(planeEquation)
-
-            // update spheres
-//            sceneView.scene.rootNode.childNodes.filter({ $0.name == "sphere" }).forEach({ $0.removeFromParentNode() })
-            if sender.state == .ended {
-                updateSlicingPlane(refNode: self.heartNode)
-                self.heartNode.opacity = 0
-            }
         }
     }
     
-    @objc func pinchGesture(sender: UIPinchGestureRecognizer) {
-        if decodeTube != nil && sender.state == .changed {
-//            print("scale", sender.scale)
-//            print("velocity", sender.velocity)
-            let pinchScaleX = Float(sender.scale) * decodeTube!.scale.x
-            let pinchScaleY =  Float(sender.scale) * decodeTube!.scale.y
-            let pinchScaleZ =  Float(sender.scale) * decodeTube!.scale.z
-            decodeTube!.scale = SCNVector3(pinchScaleX, pinchScaleY, pinchScaleZ)
-            sender.scale = 1
-            
-            sceneView.scene.rootNode.childNodes.filter({ $0.name == "sphere" }).forEach({ $0.removeFromParentNode() })
-            decodeGeom(node: decodeTube)
-        }
-    }
+//    @objc func pinchGesture(sender: UIPinchGestureRecognizer) {
+//        if decodeTube != nil && sender.state == .changed {
+////            print("scale", sender.scale)
+////            print("velocity", sender.velocity)
+//            let pinchScaleX = Float(sender.scale) * decodeTube!.scale.x
+//            let pinchScaleY =  Float(sender.scale) * decodeTube!.scale.y
+//            let pinchScaleZ =  Float(sender.scale) * decodeTube!.scale.z
+//            decodeTube!.scale = SCNVector3(pinchScaleX, pinchScaleY, pinchScaleZ)
+//            sender.scale = 1
+//        }
+//    }
     
     func placeTube() {
         if let currentFrame = sceneView.session.currentFrame {
             let tubeGeom = SCNTube(innerRadius: 0.1, outerRadius: 0.2, height: 0.3)
-            self.decodeTube = SCNNode(geometry: tubeGeom)
+            self.tubeNode = SCNNode(geometry: tubeGeom)
 
 //            self.decodeTube.geometry?.firstMaterial?.diffuse.contents = UIColor.green
-            self.decodeTube.name = "tubeNode"
+//            self.tubeNode.name = "tubeNode"
 
             //Add node set distance in front of camera
             var translation = matrix_identity_float4x4
@@ -168,9 +143,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             translation.columns.3.y = 0
             translation.columns.3.z = -0.5
             let transform = simd_mul(currentFrame.camera.transform, translation)
-            self.decodeTube.worldPosition = SCNVector3Make(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
+            self.tubeNode.worldPosition = SCNVector3Make(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
             
-            sceneView.scene.rootNode.addChildNode(self.decodeTube)
+            sceneView.scene.rootNode.addChildNode(self.tubeNode)
         }
     }
     
@@ -216,245 +191,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         }
     }
     
-    // MARK: - Slicing functions
-    
-    // from https://stackoverflow.com/questions/17250501/extracting-vertices-from-scenekit
-    func decodeGeom(node: SCNNode, scale: Float = 1) -> SCNNode? {
-        let start = CFAbsoluteTimeGetCurrent()
-        
-        // 1% took around 5 seconds
-        // 5% took 156 seconds
-        // 10% took 11 minutes (660 seconds)
-        
-        let vertices = decodeVertices(node: node)
-        let indices = decodeIndices(node: node)
-        var normals = decodeNormal(node: node)
-        
-        if vertices == nil || indices == nil {
-            print("Couldn't decode geometry")
-            return nil
-        }
-    
-        
-        let transVert = vertices!.map{SCNVector3Make($0.x * scale, $0.y * scale, $0.z * scale)}
-        
-        let returnSlice = slice(vertices: transVert, transform: node.simdTransform, plane: self.planeEquation, indices: indices!, normals: normals!)
-        
-        let slicedVertices = returnSlice.0
-        let slicedIndices = returnSlice.1
-        let slicedNormals = returnSlice.2
-        
-        
-        // Code below is to make an SCNNode of the sliced object
-        
-        let slicedSource = SCNGeometrySource(vertices: slicedVertices)
-        let slicedElements = SCNGeometryElement(indices: slicedIndices, primitiveType: (node.geometry?.elements.first?.primitiveType)!)
-        let slicedNormalsSource = SCNGeometrySource(normals: slicedNormals)
-        
-        // Right now this doesn't use the normalSource. Add slicedNormalSource to the sources array to use it
-//        let slicedGeometry = SCNGeometry(sources: [slicedSource], elements:[slicedElements])
-        let slicedGeometry = SCNGeometry(sources: [slicedSource, slicedNormalsSource], elements: [slicedElements])
-
-        
-        // Code below is to make an SCNNode of the original object
-        
-        // make scn node from decomposed source and elements
-//        let source = SCNGeometrySource(vertices: transVert)
-//        let normalSource = SCNGeometrySource(normals: normals!)
-//        let element = SCNGeometryElement(indices: indices!, primitiveType: (node.geometry?.elements.first?.primitiveType)!)
-//
-//        let geometry = SCNGeometry(sources: [source, normalSource], elements: [element])
-        
-                
-        // To show the original object: return SCNNode(geometry: geometry)
-        // To show the sliced object: return SCNNode(geometry: geometry)
-        
-        // add name to node for easier deletion later
-        let slicedNode = SCNNode(geometry: slicedGeometry)
-        slicedNode.name = "slicedNode"
-        
-        let diff = CFAbsoluteTimeGetCurrent() - start
-        print("Took \(diff) seconds")
-        
-        return slicedNode
-        
-        // sceneView.scene.rootNode.addChildNode(self.decodeTube)
-
-    }
-    
-    func decodeVertices(node: SCNNode) -> [SCNVector3]? {
-        let nodeSources = node.geometry?.sources(for: SCNGeometrySource.Semantic.vertex)
-        if let nodeSource = nodeSources?.first {
-            let stride = nodeSource.dataStride
-            let offset = nodeSource.dataOffset
-            let componentsPerVector = nodeSource.componentsPerVector
-            let bytesPerVector = componentsPerVector * nodeSource.bytesPerComponent
-
-            let vectors = [SCNVector3](repeating: SCNVector3Zero, count: nodeSource.vectorCount)
-            let vertices = vectors.enumerated().map({
-                (index: Int, element: SCNVector3) -> SCNVector3 in
-                let vectorData = UnsafeMutablePointer<Float>.allocate(capacity: componentsPerVector)
-                let byteRange = Range(NSMakeRange(index * stride + offset, bytesPerVector))
-                let buffer = UnsafeMutableBufferPointer(start: vectorData, count: componentsPerVector)
-                nodeSource.data.copyBytes(to: buffer, from: byteRange)
-                return SCNVector3Make(buffer[0], buffer[1], buffer[2])
-            })
-            
-            return vertices
-        }
-        return nil
-    }
-    
-    func decodeNormal(node: SCNNode) -> [SCNVector3]? {
-        let nodeSources = node.geometry?.sources(for: SCNGeometrySource.Semantic.normal)
-        if let nodeSource = nodeSources?.first {
-            let stride = nodeSource.dataStride
-            let offset = nodeSource.dataOffset
-            let componentsPerVector = nodeSource.componentsPerVector
-            let bytesPerVector = componentsPerVector * nodeSource.bytesPerComponent
-
-            let vectors = [SCNVector3](repeating: SCNVector3Zero, count: nodeSource.vectorCount)
-            let vertices = vectors.enumerated().map({
-                (index: Int, element: SCNVector3) -> SCNVector3 in
-                let vectorData = UnsafeMutablePointer<Float>.allocate(capacity: componentsPerVector)
-                let byteRange = Range(NSMakeRange(index * stride + offset, bytesPerVector))
-                let buffer = UnsafeMutableBufferPointer(start: vectorData, count: componentsPerVector)
-                nodeSource.data.copyBytes(to: buffer, from: byteRange)
-                return SCNVector3Make(buffer[0], buffer[1], buffer[2])
-            })
-            
-            return vertices
-        }
-        return nil
-    }
-    
-    func decodeIndices(node: SCNNode) -> [UInt32]? {
-        let nodeElements = node.geometry?.elements
-        print(nodeElements!.count)
-        var appendTo: [UInt32] = []
-        for nodeElement in nodeElements! {
-            let bytesPerIndex = nodeElement.bytesPerIndex
-            let primitiveType = nodeElement.primitiveType
-            let primitiveCount = nodeElement.primitiveCount
-            
-            // only decodes indices for triangles
-            if primitiveType == SCNGeometryPrimitiveType.triangles {
-                // 3 is used because of triangles
-                let indexVector = [UInt32](repeating: 0, count: primitiveCount * 3)
-                let indices = indexVector.enumerated().map({
-                    (index: Int, element: UInt32) -> UInt32 in
-                    let indexData = UnsafeMutablePointer<UInt32>.allocate(capacity: 1)
-                    let byteRange = Range(NSMakeRange(index * bytesPerIndex, bytesPerIndex))
-                    let buffer = UnsafeMutableBufferPointer(start: indexData, count: 1)
-                    nodeElement.data.copyBytes(to: buffer, from: byteRange)
-                    return UInt32(buffer[0])
-                })
-                for ind in indices{
-                    appendTo.append(ind)
-                }
-            }
-        }
-        return appendTo
-    }
-    
-    func slice(vertices: [SCNVector3], transform: simd_float4x4, plane: simd_float4, indices: [UInt32], normals: [SCNVector3]) -> ([SCNVector3], [UInt32], [SCNVector3]) {
-       
-        // Part 1: Array of Triangles is an array of array of three vertices that form a triangle.
-        // It converts triplets of indices to triplets of vertices
-        
-        var arrayOfTriangles = [[SCNVector3]]()
-        for i in stride(from: 0, to: indices.count, by: 3){
-            var triangle = [SCNVector3]()
-            for j in stride(from: i, to: i + 3, by: 1){
-                // TO-DO Fix index out of range error that sometimes occurs here
-                triangle.append(vertices[Int(indices[j])])
-            }
-            arrayOfTriangles.append(triangle)
-        }
-
-        
-        // Part 2: choosing which vertices are chosen for slicing and keeping track of the deleted ones
-        // Big assumption: the order of indices of normals correspond to the order of indices of vertices
-        // Adds the normals when the vertices are added
-        
-        var newVertices = [SCNVector3]()
-        var newNormals = [SCNVector3]()
-        var deletedVertices = [SCNVector3]()
-        var counter = 0
-        for vertex in vertices {
-            let tempVec = simd_float4(x: vertex.x, y: vertex.y, z: vertex.z, w: 1)
-            var worldVec = transform * tempVec
-            worldVec.w = -1
-            
-            let distance = simd_dot(worldVec, plane)
-            
-            if distance < 0 {
-                // plotRedSphere(radius: 0.025, worldPos: SCNVector3Make(worldVec.x, worldVec.y, worldVec.z))
-                
-                // The code below makes sure that vertices are only added if they are not already in the
-                // array. Having no repeats is important for part 4 to work.
-                
-                var addVertex = true
-                for vertexCheck in newVertices {
-                    if vertexCheck.x == vertex.x && vertexCheck.y == vertex.y && vertexCheck.z == vertex.z {
-                        addVertex = false
-                        break
-                    }
-                }
-                if addVertex {
-                    newVertices.append(vertex)
-                    newNormals.append(normals[counter])
-                }
-
-            }
-            else {
-                deletedVertices.append(vertex)
-            }
-            counter += 1
-        }
-
-        
-        // Part 3: Removes any triangles which contain a deleted vertex from part 2 as any of its vertices
-        
-        for i in stride(from: arrayOfTriangles.count - 1, to: 0, by: -1) {
-            let arrayAtIndex = arrayOfTriangles[i]
-            
-            // To do: Replace the bool part with a break statement to break out of the deletedVertex for loop
-            
-            var bool = true
-            for deletedVertex in deletedVertices {
-                if bool {
-                    for elem in arrayAtIndex {
-                        if elem.x == deletedVertex.x && elem.y == deletedVertex.y && elem.z == deletedVertex.z {
-                            arrayOfTriangles.remove(at: i)
-                            bool = false
-                        }
-                    }
-                }
-            }
-        }
-
-        
-        // Part 4: Creates an array of indices based off of the array of triangles.
-        // Converts from triplets of vertices to triplets of indices, but they're not array in arrays
-        
-        var newIndices = [UInt32]()
-        for triangle in arrayOfTriangles {
-            for elem in triangle {
-                var count = 0
-                for vertex in newVertices {
-                    if vertex.x == elem.x && vertex.y == elem.y && vertex.z == elem.z {
-                        newIndices.append(UInt32(count))
-                    }
-                    count += 1
-                }
-            }
-        }
-    
-        
-        return (newVertices, newIndices, newNormals)
-    }
-    
     func obj2SCNNode(name: String) -> SCNNode? {
         if let url = Bundle.main.url(forResource: name, withExtension: "obj", subdirectory: "art.scnassets") {
 //            let asset = MDLAsset(url: url)
@@ -495,45 +231,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         placeHeart()
         placePlane()
         
-//        let node = decodeGeom(node: self.decodeTube, scale: 0.33)!
-//        sceneView.scene.rootNode.addChildNode(node)
-//        node.transform = self.decodeTube.transform
-//
-//        self.decodeTube.opacity = 0
-        
-//        let node = decodeGeom(node: self.heartNode)!
-//        sceneView.scene.rootNode.addChildNode(node)
-//        node.transform = self.heartNode.transform
-//
-//        self.heartNode.opacity = 0
-        
         //create a light node
         sceneView.scene.rootNode.addChildNode(self.createLightNode()!)
-                
-//        let initTubeNode = sceneView.scene.rootNode.childNode(withName: "tubeNode", recursively: false)!
-//
-//        self.decodeTube = decodeGeom(node: initTubeNode, scale: 0.33)
-//        sceneView.scene.rootNode.addChildNode(self.decodeTube)
-//        self.decodeTube.transform = initTubeNode.transform
-//
-//        sceneView.scene.rootNode.childNodes.filter({ $0.name == "tubeNode" }).forEach({ $0.removeFromParentNode() })
-//        sceneView.scene.rootNode.addChildNode(self.decodeTube)
-        
-//        heartNode = obj2SCNNode(name: "dTGA_reduced")
-//
-//        if let currentFrame = sceneView.session.currentFrame {
-//            //Add node set distance in front of camera
-//            var translation = matrix_identity_float4x4
-//            translation.columns.3.x = 0
-//            translation.columns.3.y = 0
-//            translation.columns.3.z = -0.5
-//            let transform = simd_mul(currentFrame.camera.transform, translation)
-//            heartNode?.worldPosition = SCNVector3Make(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
-//
-//            sceneView.scene.rootNode.addChildNode(heartNode!)
-//        }
-        
-//        decodeGeom(node: self.heartNode)
     }
     
     // Creating an adjustable light SCNNode
